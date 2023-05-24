4f53408ed7dddbe1fdb16509a463aca9522b14f8
Josh Felmeden <josh.felmeden@capgemini.com>
2023-05-24 14:24:23 +0100

    added inital maaxboard support to builder + hello example

--- build_sdk.py
+++ build_sdk.py
@@ -85,6 +85,19 @@ SUPPORTED_BOARDS = (
         examples = {
             "ethernet": Path("example/imx8mm/passive_server")
         }
+    ),
+    BoardInfo(
+        name="maaxboard",
+        gcc_cpu="cortex-a53",
+        loader_link_address=0x40480000,
+        kernel_options = {
+            "KernelPlatform": "imx8mq-evk",
+            "KernelIsMCS": True,
+            "KernelArmExportPCNTUser": True,
+        },
+        examples = {
+            "hello": Path("example/maaxboard/hello")
+        }
     )
 )
 

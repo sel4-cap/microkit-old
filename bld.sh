a279c84fb30e85d6910d946d21bec65cb5840a49
mcossins <matthew.cossins@capgemini.com>
2023-07-24 10:51:33 +0100

    MS

--- bld.sh
+++ bld.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILE_LOCATION="/var/lib/tftpboot/loader-josh.img"
+FILE_LOCATION="/var/lib/tftpboot/loader-matt.img"
 export SEL4CP_SDK="$HOME/cp/sel4cp/release/sel4cp-sdk-1.2.6"
 export PATH="$HOME/cp/compiler_download/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf/bin:$PATH"
 rm -r $HOME/cp/sel4cp/tmp_build

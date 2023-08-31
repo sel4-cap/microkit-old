52614f15f4d3afbeaa1fadc295ea1f26d590f0e5
Josh Felmeden <josh.felmeden@capgemini.com>
2023-08-31 16:32:26 +0100

    can send keyboard keypress over ethernet. Tempramental

--- build_ben.sh
+++ build_ben.sh
@@ -1,7 +1,8 @@
 export SEL4CP_SDK="$HOME/cp/sel4cp/release/sel4cp-sdk-1.2.6"
 export PATH="$HOME/cp/compiler_download/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf/bin:$PATH"
+export SEL4CP_DIR="$HOME/cp/sel4cp"
 rm -r $HOME/cp/sel4cp/tmp_build
 $HOME/cp/pyenv/bin/python3.9 dev_build.py --board=maaxboard --example=xhci_stub
-sudo cp $HOME/cp/sel4cp/tmp_build/loader.img /var/lib/tftpboot/loader-ben.img
+sudo cp $HOME/cp/sel4cp/tmp_build/loader.img /var/lib/tftpboot/loader-josh.img
 echo 
-echo Copied image file to "/var/lib/tftpboot/loader-ben.img".
+echo Copied image file to "/var/lib/tftpboot/loader-josh.img".

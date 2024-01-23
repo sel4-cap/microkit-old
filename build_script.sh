7aeba9c16c1e72e9afe9c8643ddf0dc2e90c68f2
dstorer-cap <daniel.storer@capgemini.com>
2024-01-23 15:21:01 +0000

    Modified path to compiler

--- build_script.sh
+++ build_script.sh
@@ -2,6 +2,6 @@ sudo rm -r tmp_build
 curl --output compiler.xz https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/10.2-2020.11/binrel/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf.tar.xz
 tar -xvf compiler.xz
 rm -r compiler.xz
-export PATH=gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf/bin:$PATH
+export PATH=${PWD}/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf/bin:$PATH
 python build_sdk.py --sel4 ../seL4
 

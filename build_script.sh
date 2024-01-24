aef635e1ad9367e7e0565a7223201797d6440fb8
dstorer-cap <daniel.storer@capgemini.com>
2024-01-24 13:00:19 +0000

    Updated build script to include picolibc

--- build_script.sh
+++ build_script.sh
@@ -1,7 +1,16 @@
+# Build sdk
 sudo rm -r tmp_build
 curl --output compiler.xz https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/10.2-2020.11/binrel/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf.tar.xz
 tar -xvf compiler.xz
 rm -r compiler.xz
 export PATH=${PWD}/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf/bin:$PATH
-python build_sdk.py --sel4 ../seL4
+python3 build_sdk.py --sel4 ../seL4
+# Build picolibc
+sudo rm -rf picolibc-microkit/
+sudo mkdir picolibc-microkit
+cd picolib-microkit
+mkdir ../../picolibc_build
+sudo ../scripts/do-aarch64-configure-nocrt -Dprefix=${PWD}/../../picolibc_build
+sudo ninja 
+sudo ninja install
 
